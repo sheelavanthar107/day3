49c549e04a6025ceaa594860c8a6c713602e7f20
Harshitha S <harshithah785@gmail.com>
2025-05-14 15:44:20 +0530

    Update and rename factorial.c to factorial array.c

--- factorial array.c
+++ factorial array.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int main() {
-    int n,c;
+    int n,i,c=1;
     printf("enter the value of n\n");
     scanf("%d",&n);
     if(n==0){
         printf("1");
     }
     else{
-        c= n*(n-1);
+        for(i=1;i<=n;i++){
+            c*=i;
+        }
         printf("%d",c);
 }
 }
